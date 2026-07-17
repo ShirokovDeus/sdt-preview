1b6add7e5141498bd3e8db74bb9adc698709652a
ShirokovDeus <schirokovvip@gmail.com>
2026-07-17 23:37:28 +0300

    fix broken video-tour: remove dead play button (no video file), honest photo+floorplan preview

---
+++ _next/static/chunks/app/page-27a423e906a0516a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{},function(n){n.O(0,[929,59,972,360,591,513,232,48,491,971,117,744],function(){return n(n.s=8491)}),_N_E=n.O()}]);
--- _next/static/chunks/app/page-c42dfb4268e90bd4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{},function(n){n.O(0,[929,59,972,360,169,513,232,48,491,971,117,744],function(){return n(n.s=8491)}),_N_E=n.O()}]);
